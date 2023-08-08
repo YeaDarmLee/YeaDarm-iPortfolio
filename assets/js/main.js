ca04380cccdee1d3bdbd404773003dabafb7384c
YeaDarmLee <gnswpwhrqkf3@gmail.com>
2023-08-08 17:42:21 +0900

    YeaDarm-iPortfolio Data Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,16 +47,23 @@
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
+    if (position < 4900) {
+      navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+          navbarlink.classList.add('active')
+        } else {
+          navbarlink.classList.remove('active')
+        }
+      })
+    } else {
+      navbarlinks.forEach(navbarlink => {
         navbarlink.classList.remove('active')
-      }
-    })
+      })
+      navbarlinks[4].classList.add('active')
+    }
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
